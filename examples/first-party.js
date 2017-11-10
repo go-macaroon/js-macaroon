f0da70590b8bdf573d29ae540d8a3f08921bf0d9
Evan Schwartz <evan.mark.schwartz@gmail.com>
2017-11-09 23:10:18 -0500

    docs: add first party caveat usage example

--- examples/first-party.js
+++ examples/first-party.js
@@ -1,19 +1,5 @@
-# js-macaroon
-> Better than cookies! :cookie:
+'use strict'
 
-[Macaroons](http://theory.stanford.edu/~ataly/Papers/macaroons.pdf) are "Cookies with Contextual Caveats for Decentralized Authorization in the Cloud". Basically, that means bearer tokens that can be given limited scopes or expirations, and which can be limited further by users or 3rd party services themselves.
-
-This JavaScript implementation is compatible with the [Go](http://github.com/go-macaroon/macaroon), [Python, and C ](https://github.com/rescrv/libmacaroons) implementations. It also includes functionality to interact with third party caveat dischargers implemented by the [Go macaroon bakery](http://github.com/go-macaroon-bakery/macaroon-bakery).
-
-## Installation
-
-`npm install --save macaroon`
-
-## Usage
-
-Try out the code in the [examples](./examples)!
-
-```js
 const crypto = require('crypto')
 const Macaroon = require('..')
 
@@ -26,6 +12,8 @@ const original = Macaroon.newMacaroon({
 })
 const givenToUser = original.exportBinary()
 
+console.log('original macaroon binary:', Buffer.from(original.exportBinary()).toString('base64'))
+console.log('original macaroon json:', original.exportJSON())
 
 // The user can add additional caveats that will be enforced by the server
 // (The server can support whatever type of specific caveats you want)
@@ -33,7 +21,8 @@ const userMacaroon = Macaroon.importMacaroon(givenToUser)
 userMacaroon.addFirstPartyCaveat('time < ' + new Date(Date.now() + 1000).toISOString())
 const givenToThirdParty = userMacaroon.exportBinary()
 
-
+console.log('caveated macaroon binary:', Buffer.from(original.exportBinary()).toString('base64'))
+console.log('caveated macaroon json:', original.exportJSON())
 
 // The user can give out the more limited caveat to some 3rd party service
 // and the server will enforce the specified conditions when the 3rd party goes to authenticate
@@ -51,8 +40,10 @@ function verifier (caveat) {
   }
 }
 
+console.log('verifying macaroon')
 const fromThirdParty = Macaroon.importMacaroon(givenToThirdParty)
 fromThirdParty.verify(rootKey, verifier)
+console.log('yay! the macaroon was valid')
 
 // In this example, the macaroon will expire after 1 second
 setTimeout(function () {
@@ -62,4 +53,5 @@ setTimeout(function () {
     console.log('...but if we wait for too long the server won\'t accept it anymore')
   }
 }, 1001)
-```
+
+
