baaf6e722d482660b11d20594cf39200265392df
Nick Robison <nicholas.a.robison@omb.eop.gov>
2018-12-07 14:41:08 -0500

    Add null check to decrypt response
    When decryption fails due to an incorrect root key, the response from the `decrypt` function is null instead of false.

--- macaroon.js
+++ macaroon.js
@@ -505,7 +505,7 @@ const decrypt = function(keyBits, ciphertextBits) {
   const nonceBytes = ciphertextBytes.slice(0, NONCELEN);
   const dataBytes = ciphertextBytes.slice(NONCELEN);
   let textBytes = nacl.secretbox.open(dataBytes, nonceBytes, keyBytes);
-  if (textBytes === false) {
+  if (textBytes === false || textBytes === null) {
     throw new Error('decryption failed');
   }
   return bytesToBits(textBytes);
